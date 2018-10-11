c60b610693d58fae96657dc47be7f73fd4e9220d
Evandro Zilli Pavei <evzpav@gmail.com>
2018-10-11 17:40:17 -0300

    update operations with get action

--- stratumsdk/operations.go
+++ stratumsdk/operations.go
@@ -17,23 +17,23 @@ type FeeData struct {
 }
 
 type OperationData struct {
-	WalletID                  int     `json:"wallet_id"`
-	OperationID               int     `json:"operation_id"`
-	OperationAmount           float64 `json:"operation_amount"`
-	OperationTotalAmount      float64 `json:"operation_tamount"`
-	OperationFee              float64 `json:"operation_fee"`
-	OperationDescription      string  `json:"operation_desc"`
-	OperationExternalID       int     `json:"operation_eid"`
-	OperationExternalTXID     int     `json:"operation_etxid"`
-	OperationTimestamp        int     `json:"operation_ts"`
-	OperationUpdatedTimestamp int     `json:"operation_upd_ts"`
-	OperationConfirmations    int     `json:"operation_conf"`
-	OperationConfRequired     int     `json:"operation_confreq"`
-	DestinationTypeData       string  `json:"dest_type_data"`
-	OperationInfo             string  `json:"operation_info"`
-	OperationStatus           string  `json:"operation_status"`
-	OperationType             string  `json:"operation_type"`
-	DirectionType             string  `json:"direction_type"`
+	WalletId               int     `json:"wallet_id"`
+	OperationId            int     `json:"operation_id"`
+	OperationAmount        float64 `json:"operation_amount"`
+	OperationTotalAmount   float64 `json:"operation_tamount"`
+	OperationFee           float64 `json:"operation_fee"`
+	OperationDescription   string  `json:"operation_desc"`
+	OperationExternalId    int     `json:"operation_eid"`
+	OperationExternalTXId  string  `json:"operation_etxid"`
+	OperationTs            int     `json:"operation_ts"`
+	OperationUpdatedTs     int     `json:"operation_upd_ts"`
+	OperationConfirmations int     `json:"operation_conf"`
+	OperationConfRequired  int     `json:"operation_confreq"`
+	DestinationTypeData    string  `json:"dest_type_data"`
+	OperationInfo          string  `json:"operation_info"`
+	OperationStatus        string  `json:"operation_status"` //"in:new,processing,done,failed"
+	OperationType          string  `json:"operation_type"`   //"in:deposit,withdraw,transfer"
+	DirectionType          string  `json:"direction_type"`   //"in:in,out,intra"
 }
 type OperationPayload struct {
 	DestType           string `json:"dest_type,omitempty"`      // types: in,out,intra
@@ -47,11 +47,15 @@ type OperationPayload struct {
 	OperationUpdTsTo   int    `json:"operation_upd_ts_to,omitempty"`   // doubt ask for Sven
 	WalletEid          int    `json:"wallet_eid,omitempty"`
 	WalletGroupEid     int    `json:"wallet_group_eid,omitempty"`
-	WalletID           int    `json:"wallet_id,omitempty"`
-	OperationID        int    `json:"operation_id"`
+	WalletId           int    `json:"wallet_id,omitempty"`
+	OperationId        int    `json:"operation_id"`
 }
 
 type OperationResult struct {
+	Data OperationData `json:"data"`
+}
+
+type OperationListResult struct {
 	Data []OperationData `json:"data"`
 }
 
@@ -84,15 +88,31 @@ func (o *Operations) Fees(payload *FeePayload) (*[]FeeData, *ApiError, error) {
 
 }
 
-// List - list opearation in account
+// List - list operations in account
 func (o *Operations) List(payload *OperationPayload) (*[]OperationData, *ApiError, error) {
-	result := new(OperationResult)
+	result := new(OperationListResult)
 	payloadJSON, err := json.Marshal(payload)
 	if err != nil {
 		return nil, nil, err
 	}
 	fmt.Printf("%s", payloadJSON)
 	apiErr, err := o.client.call("operations", "list", payloadJSON, &result)
+	if err != nil {
+		return nil, nil, err
+	}
+	return &result.Data, apiErr, nil
+
+}
+
+// Get - get operation in account
+func (o *Operations) Get(payload *OperationPayload) (*OperationData, *ApiError, error) {
+	result := new(OperationResult)
+	payloadJSON, err := json.Marshal(payload)
+	if err != nil {
+		return nil, nil, err
+	}
+	fmt.Printf("%s", payloadJSON)
+	apiErr, err := o.client.call("operations", "get", payloadJSON, &result)
 	if err != nil {
 		return nil, nil, err
 	}
--- stratumsdk/operations_test.go
+++ stratumsdk/operations_test.go
@@ -20,12 +20,24 @@ func TestOperationFees(t *testing.T) {
 
 func TestOperationList(t *testing.T) {
 	client := stratumsdk.Initial(apiUser, apiSecret, true)
-	feePayload := new(stratumsdk.OperationPayload)
-	_, apiErr, err := client.Operations().List(feePayload)
+	operationPayload := new(stratumsdk.OperationPayload)
+	_, apiErr, err := client.Operations().List(operationPayload)
 	if err != nil {
 		t.Errorf("sdk error: TestOperationList() -> %s ", err.Error())
 	}
 	if apiErr != nil {
 		t.Errorf("apiError: TestOperationList() -> %s ", apiErr.Data)
+	}
+}
+
+func TestOperationGet(t *testing.T) {
+	client := stratumsdk.Initial(apiUser, apiSecret, true)
+	operationPayload := new(stratumsdk.OperationPayload)
+	_, apiErr, err := client.Operations().Get(operationPayload)
+	if err != nil {
+		t.Errorf("sdk error: TestOperationGet() -> %s ", err.Error())
+	}
+	if apiErr != nil {
+		t.Errorf("apiError: TestOperationGet() -> %s ", apiErr.Data)
 	}
 }
